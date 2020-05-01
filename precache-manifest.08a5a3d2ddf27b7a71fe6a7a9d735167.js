aa96affbc9e3fbb7d65289ab7393aab7891645c5
Kutabix <bartekd123456@wp.pl>
2020-05-01 03:32:42 +0200

    Updates

---
+++ precache-manifest.08a5a3d2ddf27b7a71fe6a7a9d735167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13201afc658fcc0aedf3a7840b1c2c06",
+    "url": "/gra-w-wisielca/index.html"
+  },
+  {
+    "revision": "22fd40941563d2fdda6e",
+    "url": "/gra-w-wisielca/static/css/main.5f4c1341.chunk.css"
+  },
+  {
+    "revision": "f157b3e30ac57300279a",
+    "url": "/gra-w-wisielca/static/js/2.3a7a9ee0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gra-w-wisielca/static/js/2.3a7a9ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fd40941563d2fdda6e",
+    "url": "/gra-w-wisielca/static/js/main.23b50890.chunk.js"
+  },
+  {
+    "revision": "0fd7dc4374975d95105c",
+    "url": "/gra-w-wisielca/static/js/runtime-main.09ba61c0.js"
+  }
+]);
